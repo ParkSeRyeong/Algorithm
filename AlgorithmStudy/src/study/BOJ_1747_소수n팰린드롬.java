39790707ec41ea60c52c88522c2b8fd49908911d
ParkSeRyeong <63963540+ParkSeRyeong@users.noreply.github.com>
2021-04-05 22:23:07 +0900

    04.05 알고리즘 연습

---
+++ AlgorithmStudy/src/study/BOJ_1747_소수n팰린드롬.java
@@ -0,0 +1,54 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class BOJ_1747_소수n팰린드롬 {
+	static ArrayList<Integer> list = new ArrayList<>();
+
+	public static boolean[] getPrimes() {
+		boolean[] primes = new boolean[1003002];
+		primes[1] = true;
+
+		for (int i = 2; i < 1003002; ++i) {
+			if (primes[i])
+				continue; // 소수가 아닌(true) 수는 넘어가기
+
+			for (int j = i + i; j < 1003002; j += i) { // i를 제외한 i의 배수 모두 체크하기
+				primes[j] = true;
+			}
+			list.add(i);
+		}
+
+		return primes;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+
+		boolean[] primes = getPrimes();
+
+		Iterator<Integer> it = list.iterator();
+		while (it.hasNext()) {
+			int now=it.next();
+			String str = String.valueOf(now);
+			boolean flag=true;
+			for (int i = 0; i < str.length() / 2; i++) {
+				if(str.charAt(i)!=str.charAt(str.length()-i-1)) {
+					flag=false;
+					break;
+				}
+			}
+			if(flag && now>=N) {
+				System.out.println(now);
+				return;
+			}
+			
+		}
+		System.out.println(list.size());
+	}
+}
--- AlgorithmStudy/src/study/BOJ_9177_단어섞기.java
+++ AlgorithmStudy/src/study/BOJ_9177_단어섞기.java
@@ -1,5 +1,69 @@
 package study;
 
-public class BOJ_9177_단어섞기 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class BOJ_9177_단어섞기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		f: for (int t = 1; t <= N; t++) {
+			String[] tmp = br.readLine().split(" ");
+			char[] a = tmp[0].toCharArray();
+			char[] b = tmp[1].toCharArray();
+			char[] c = tmp[2].toCharArray();
+
+			Queue<Character> q = new LinkedList<>();
+			for (int i = 0; i < c.length; i++) {
+				q.offer(c[i]);
+			}
+//			System.out.println("*********************");
+//			System.out.println(Arrays.toString(c));
+//			System.out.println("*********************");
+			int pa = 0;
+			int pb = 0;
+			int cnt = 0;
+			while (!q.isEmpty()) {
+				System.out.println("-------------");
+				System.out.println("현재 비교 : " + q.peek());
+				// 둘 다 똑같으면 cnt 증가.
+				if(pa+cnt<a.length&&pb+cnt<b.length) {
+					if (a[pa+cnt] == q.peek() && b[pb+cnt] == q.peek()) {
+						System.out.println("앗 둘 다 똑같다 : "+pa+" "+pb);
+						cnt++;
+						q.poll();
+						continue;
+					}
+				}
+				// 둘 중 하나와만 같다면?
+				System.out.println("현재 cnt : "+cnt);
+				if (pa+cnt < a.length && a[pa+cnt] == q.peek()) {
+					if (cnt != 0) {
+						System.out.println("\tpa :" + pa+" -> "+(pa+cnt));
+						pa += cnt;
+						cnt = 0;
+					}
+					pa++;
+				} else if (pb+cnt < b.length && b[pb+cnt] == q.peek()) {
+					if (cnt != 0) {
+						System.out.println("\tpb :" + pb+" -> "+(pb+cnt));
+						pb += cnt;
+						cnt = 0;
+					}
+					pb++;
+				} else {
+					sb.append("Data set ").append(t).append(": no\n");
+					continue f;
+				}
+				q.poll();
+			}
+			sb.append("Data set ").append(t).append(": yes\n");
+		}
+		System.out.println(sb.toString());
+	}
 }
