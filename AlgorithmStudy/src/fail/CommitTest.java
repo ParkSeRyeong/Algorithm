4765b5e6c3855687184263d19ab6d43607e3574e
ParkSeRyeong <63963540+ParkSeRyeong@users.noreply.github.com>
2022-09-10 17:28:21 +0900

    커밋 테스트 #4

--- AlgorithmStudy/src/fail/CommitTest.java
+++ AlgorithmStudy/src/fail/CommitTest.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("commit test #1");
 	System.out.println("commit test #2");
 	System.out.println("commit test #3");
+	System.out.println("commit test #4");
 }
 }
